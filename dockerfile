9a2fcd046a345c89885ac8e9c48b3941f6d8b0ae
Deezinn <developer.andrelz@gmail.com>
2025-04-16 00:52:04 -0300

    👀 started!

--- dockerfile
+++ dockerfile
@@ -1,15 +1,29 @@
+# Use Alpine como base
 FROM alpine:3.21
 
+# Defina o diretório de trabalho
 WORKDIR /app
 
+# Instale as dependências necessárias (Python, pip, etc.)
 RUN apk add --no-cache python3 py3-pip py3-virtualenv
 
+# Crie o ambiente virtual
 RUN python3 -m venv /venv
 
+# Defina o diretório do ambiente virtual como variável de ambiente
+ENV PATH="/venv/bin:$PATH"
+
+# Copie o arquivo requirements.txt para o container
 COPY requirements.txt .
 
-RUN /venv/bin/pip install --no-cache-dir -r requirements.txt
+# Instale as dependências Python no ambiente virtual
+RUN pip install --no-cache-dir -r requirements.txt
 
+# Copie o código da aplicação
 COPY src/ .
 
-CMD ["/venv/bin/python", "index.py"]
+# Exponha a porta usada pelo Dash (default é 8050)
+EXPOSE 8050
+
+# Defina o comando para rodar o app
+CMD ["python", "index.py"]
---
+++ terraform/main.tf
@@ -0,0 +1,47 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "62a47bad-99aa-450d-a7e2-ef75d08015f4"  # Sua Subscription ID
+}
+
+# Criar o grupo de recursos
+resource "azurerm_resource_group" "rg" {
+  name     = "my-dash-app-rg"
+  location = "East US"
+}
+
+# Criar o plano de serviço (Linux para Docker) com SKU F1
+resource "azurerm_service_plan" "app_service_plan" {
+  name                = "my-dash-app-service-plan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku_name            = "B1"  # SKU F1 (Plano gratuito)
+  os_type             = "Linux"  # Tipo de sistema operacional (Linux)
+
+  tags = {
+    environment = "production"
+  }
+}
+
+# Criar o App Service que vai rodar o container
+resource "azurerm_linux_web_app" "app" {
+  name                = "my-dash-web-app"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.app_service_plan.id
+
+  site_config {
+    # Remover linux_fx_version, e usar a configuração customizada para o Docker
+    app_command_line = "/bin/sh -c 'python index.py'"  # Comando para rodar sua aplicação no container
+  }
+
+  app_settings = {
+    "WEBSITES_PORT"            = "8050"  # Porta onde o app vai rodar
+    "DOCKER_CUSTOM_IMAGE_NAME" = "deezinn/my-dash-app:latest"  # URL da sua imagem Docker no Docker Hub
+  }
+
+  tags = {
+    environment = "production"
+  }
+
+  depends_on = [azurerm_service_plan.app_service_plan]
+}
